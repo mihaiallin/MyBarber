3bc1427ad91499396b119d8654fc72a25bd46d33
mihaiallin <mihaialin134@yahoo.com>
2024-02-05 15:21:04 +0200

    add errorpage and Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import './App.css';
+import ErrorPage from "./pages/ErrorPage";
 import Register from './pages/Register';
+// import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+    </Router>
+    </div>
     </div>
   );
 }
