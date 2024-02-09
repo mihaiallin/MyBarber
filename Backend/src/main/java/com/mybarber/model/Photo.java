87032bed45d84b931f3095df78dbd9b9bd914432
mihaiallin <mihaialin134@yahoo.com>
2024-02-10 00:44:05 +0200

    test

--- Backend/src/main/java/com/mybarber/model/Photo.java
+++ Backend/src/main/java/com/mybarber/model/Photo.java
@@ -1,28 +1,28 @@
-package com.mybarber.model;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.Date;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Entity
-@Table(name= "photos")
-public class Photo {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name= "barber_id", nullable = false)
-    private User barber;
-
-    private String photoUrl;
-    private String description;
-    private Date uploadDate;
-}
+//package com.mybarber.model;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.util.Date;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//@Entity
+//@Table(name= "photos")
+//public class Photo {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne
+//    @JoinColumn(name= "barber_id", nullable = false)
+//    private User barber;
+//
+//    private String photoUrl;
+//    private String description;
+//    private Date uploadDate;
+//}
--- Backend/src/main/java/com/mybarber/repository/RatingRepository.java
+++ Backend/src/main/java/com/mybarber/repository/RatingRepository.java
@@ -1,11 +1,11 @@
-package com.mybarber.repository;
-
-import com.mybarber.model.Photo;
-import com.mybarber.model.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RatingRepository extends JpaRepository<Rating, Long> {
-
-}
+//package com.mybarber.repository;
+//
+//import com.mybarber.model.Photo;
+//import com.mybarber.model.Rating;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface RatingRepository extends JpaRepository<Rating, Long> {
+//
+//}
